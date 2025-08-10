09ee2e7ca24981dc74e805582dbb2b9f447a2794
Auspexi <hopiumcalculator@gmail.com>
2025-08-10 18:19:45 +0100

    Index, layout, footer, header update.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div 
-      className="flex flex-col min-h-screen bg-gradient-to-b from-slate-950 via-slate-900 to-slate-950 text-slate-100"
+      className="flex flex-col min-h-screen bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900 text-slate-100"
       style={{ margin: 0, padding: 0 }}
     >
       <Header />
